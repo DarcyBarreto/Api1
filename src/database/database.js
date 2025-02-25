eb76cf26fd50ee8db969e9c969b1711a49cebfb0
DarcyBarreto <darcybarreto02.28@gmail.com>
2025-02-25 16:22:30 -0500

    Docker

--- src/database/database.js
+++ src/database/database.js
@@ -5,14 +5,8 @@ export const sequelize = new Sequelize(
   "postgres", // username
   "12345", // password
   {
-    host: "localhost",
+    host: "apinode_db",
     dialect: "postgres",
-    // pool: {
-    //   max: 5,
-    //   min: 0,
-    //   require: 30000,
-    //   idle: 10000,
-    // },
-    // logging: false,
+ 
   }
 );

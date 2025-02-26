f1a88020c4697471b655e18ba8945116fc816ac8
DarcyBarreto <darcybarreto02.28@gmail.com>
2025-02-26 16:36:34 -0500

    Docker modificado

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,15 @@
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const sequelize = new Sequelize(
-  "api", // db name,
-  "postgres", // username
-  "12345", // password
-  {
-    host: "apinode_db",
-    dialect: "postgres",
- 
-  }
+    process.env.DB_NAME || 'api',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || '12345',
+    {
+    host: process.env.DB_HOST || 'database',
+    dialect: 'postgres',
+    logging: false,
+    port: process.env.DB_PORT || 5432,
+    }
 );
